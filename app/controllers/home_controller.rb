e225de7d05fcf1baf66e34bff4659f531fb76d7a
D.Coloma <dnl.coloma@gmail.coma>
2018-02-15 15:21:11 +0100

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
 def home
 end
   def tweet
-    @tweet = SendTweet.new(params[:tweet][:message]).perform
+    #@tweet =
+    SendTweet.new(params[:tweet][:message]).perform
 
   end
 
--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -5,20 +5,20 @@ class SendTweet
   end
 
   def perform
-    #log_in_to_twitter.update(@tweet)
-    return ENV['TWITTER_API_KEY']
+    log_in_to_twitter.update(@tweet)
+
   end
 
   def log_in_to_twitter
-=begin
+
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = TWITTER_API_KEY
-      config.consumer_secret     = TWITTER_API_SECRET
-      config.access_token        = TWITTER_ACCESS_TOKEN
-      config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+    config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
     client
-=end
+
 
   end
 
